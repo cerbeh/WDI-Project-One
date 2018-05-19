39497c744e784e7c086866c86620719474de052a
Martin Allgood <martintallgood@gmail.com>
2018-05-19 12:45:41 +0100

    Changed the forEach on grid array to instead loop. Didnt fix the problem of the loop setting the value of every object on the grid

--- js/app.js
+++ js/app.js
@@ -17,51 +17,54 @@ const objectValues = {
 };
 
 //generates a 10x10 grid with objects stored inside it.
-const characterGrid = Array(10).fill(Array(10).fill(generateObjects()));
+const characterGrid = Array(10).fill(Array(10).fill(objectValues));
 
 
 
-function populateFirstRow(array) {
-
-
-  //changing the character value of the object. But i'm only generating one random number.
-  array[0].forEach(function(element) {
-    const object = generateObjects();
-    console.log(object.characterValue);
 
     //There should be a this somewhere so that the number is only being assigned to the object you're in.
     //You're generating 10random numbers fine but you're only storing the last one.
 
-    array[0][0].characterValue = object.characterValue;
     //console.log(element.characterValue);
-  });
+}
 
   //console.log($rowElements);
 
+  //
+  // const $rowElements = $("div[rowNumber*='0']");
+  // $rowElements.each(function(element) {
+  //   //console.log(element.characterValue);
+  //   switch (element.characterValue) {
+  //     case 0:
+  //     case 1:
+  //     case 2:
+  //       $rowElements.attr('class', 'blank');
+  //       break;
+  //     case 3:
+  //       $rowElements.attr('class', 'neutral');
+  //       break;
+  //     case 4:
+  //       $rowElements.attr('class', 'kill');
+  //       break;
+  //     case 5:
+  //       $rowElements.attr('class', 'save');
+  //       break;
+  //   }
+  //   //$rowElements.appendTo('#map');
+  // });
 
-  const $rowElements = $("div[rowNumber*='0']");
-  $rowElements.each(function(element) {
-    //console.log(element.characterValue);
-    switch (element.characterValue) {
-      case 0:
-      case 1:
-      case 2:
-        $rowElements.attr('class', 'blank');
-        break;
-      case 3:
-        $rowElements.attr('class', 'neutral');
-        break;
-      case 4:
-        $rowElements.attr('class', 'kill');
-        break;
-      case 5:
-        $rowElements.attr('class', 'save');
-        break;
+  function populateFirstRow(array) {
+
+
+    //changing the character value of the object. But i'm only generating one random number.
+    //array[0].forEach(function(element) { """"
+
+    for (let i = 0; i < array[0].length; i++){
+      const object = generateObjects();
+      console.log(object.characterValue);
+      array[0][i].characterValue = object.characterValue;
     }
-    //$rowElements.appendTo('#map');
-  });
 
-}
 
 $(()=>{
 
